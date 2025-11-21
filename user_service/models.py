743f93f325db26c51a95568f21423e7b83d8ad1a
giorgosgrv3 <giorgosgrv3@gmail.com>
2025-11-21 20:09:19 +0200

    issue fix

--- user_service/models.py
+++ user_service/models.py
@@ -10,12 +10,13 @@ class Role(str, enum.Enum):
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True, index=True, nullable=False)
+    username = Column(String(50), primary_key=True, index=True, nullable=False)
     email = Column(String(100), unique=True, index=True, nullable=False)
     password_hash = Column(String(255), nullable=False)
     first_name = Column(String(50))
     last_name = Column(String(50))
+    role = Column(SAEnum(Role), default=Role.MEMBER)
+    active = Column(Boolean, default=False)
     
     # Χρησιμοποιούμε SAEnum για ασφάλεια
     role = Column(SAEnum(Role), default=Role.MEMBER)
